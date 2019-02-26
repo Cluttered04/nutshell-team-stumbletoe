3b489d4cb87bab1f18f8edd6edecd4aaa53999d2
Newforce <nicholasandrewmitchell@gmail.com>
2019-02-26 11:41:02 -0500

    merging with new master

--- src/scripts/chat/createMessage.js
+++ src/scripts/chat/createMessage.js
@@ -1,10 +1,8 @@
-function chatManager(activeUser) {
-  let messageHTML = "";
-  fetch(`http://localhost:8088/messages?_expand=user`)
-    // ?userId=${activeUser}
-    .then(messages => messages.json())
-    .then(messages => {
-      for (let i = messages.length; i--;) {
+
+let messageHTML = "";
+
+function createMessage(activeUser){
+    for (let i = messages.length; i--;) {
         if (messages[i].userId === activeUser) {
           messageHTML += `<h4 class="name-chat-${messages[i].id}">${messages[i].user.username}</h4><p class="msg-chat-${messages[i].id}">${
             messages[i].message
@@ -14,8 +12,8 @@ function chatManager(activeUser) {
           messageHTML += `<h4 class="name-chat-${messages[i].id}">${messages[i].user.username}</h4><p class="msg-chat-otherUser">${messages[i].message}</p>`;
           document.getElementById("chat-cont").innerHTML = `${messageHTML}`;
         }
-      }
-    });
+    }
+    return
 }
 
-export default chatManager;
+export default createMessage;
