57897b9333ab7a369ae65912dcfa28873bc43a39
Newforce <nicholasandrewmitchell@gmail.com>
2019-02-26 10:38:40 -0500

    Merging master

--- src/scripts/chat/chatManager.js
+++ src/scripts/chat/chatManager.js
@@ -1,15 +1,21 @@
 function chatManager(activeUser) {
-    let messageHTML = "";
-    fetch(
-      `http://localhost:8088/messages?userId=${activeUser}`
-    )
-      .then(messages => messages.json())
-      .then(messages => {
-        for (let i = 0; i < messages.length; i++) {
-          messageHTML += `<p>${messages[i].message}</p>`
-          document.getElementById("chat-cont").innerHTML = `${messageHTML}`
+  let messageHTML = "";
+  fetch(`http://localhost:8088/messages`)
+    // ?userId=${activeUser}
+    .then(messages => messages.json())
+    .then(messages => {
+      for (let i = 0; i < messages.length; i++) {
+        if (messages[i].userId === activeUser) {
+          messageHTML += `<p class="msg-chat-${messages[i].id}">${
+            messages[i].message
+          }</p>`;
+          document.getElementById("chat-cont").innerHTML = `${messageHTML}`;
+        } else {
+          messageHTML += `<p class="msg-chat-otherUser">${messages[i].message}</p>`;
+          document.getElementById("chat-cont").innerHTML = `${messageHTML}`;
         }
-      });
-  }
+      }
+    });
+}
 
-  export default chatManager;
+export default chatManager;
