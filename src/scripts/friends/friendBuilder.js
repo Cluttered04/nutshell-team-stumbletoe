f4814f37c012d5692b7e0df33f5b1506d06f13eb
sydneywait <sydneywait@gmail.com>
2019-02-28 14:02:44 -0500

    added comments and removed excess console logs

--- src/scripts/friends/friendBuilder.js
+++ src/scripts/friends/friendBuilder.js
@@ -6,35 +6,34 @@ import APIManager from "./friendAPIManager"
 
 const buildFriends = (userId) => {
     let htmlString = "<h2>Friends:</h2>"
+    // Get all the friends when the user is in the otherFriendId place
     APIManager.getAllFriendsByFriend(userId)
         .then((friends) => {
-            console.log("friends inside of friend by friend", friends)
-            // debugger;
+            // If there are any friends assigned to the user, continue
             if (friends.length > 0) {
 
                 friends.forEach(friend => {
                     htmlString += `<h4>${friend.user.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friend.id}">delete</button>`
                     document.querySelector("#frnds-list").innerHTML = htmlString;
-                    console.log("inside for loop1")
                 })
-
+                // get all the friends when the user is in the userId place
                 APIManager.getAllFriendsByUser(userId)
                     .then((friends) => {
-                        console.log("friends inside of friend by user", friends)
+                        // Check to see if there are any. If so, get their Ids
                         if (friends.length > 0) {
                             friends.forEach(friend => {
                                 const otherFriendId = friend.otherFriendId
                                 const friendshipId = friend.id
-
+                                // use the ids to get them individually
                                 APIManager.getSingleFriendbyId(otherFriendId)
                                     .then((singleFriend) => {
                                         htmlString += `<h4>${singleFriend.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friendshipId}">delete</button>`
-                                        // console.log(htmlString)
                                         document.querySelector("#frnds-list").innerHTML = htmlString;
                                     })
                             })
                         }
                         else {
+                            // otherwise, leave the HTML string unchanged
 
                             htmlString = htmlString
                             document.querySelector("#frnds-list").innerHTML = htmlString;
@@ -44,22 +43,23 @@ const buildFriends = (userId) => {
 
             }
             else {
+                // If the user is never in the otherFriendId place, check the userId place by itself
                 APIManager.getAllFriendsByUser(userId)
                     .then(friends => {
-                        console.log("friends inside of friend by user", friends)
+                        // if they have friends, get ids
                         if (friends.length > 0) {
                             friends.forEach(friend => {
                                 const otherFriendId = friend.otherFriendId
                                 const friendshipId = friend.id
-
+                                // use ids to look up friends and make a list
                                 APIManager.getSingleFriendbyId(otherFriendId)
                                     .then((singleFriend) => {
                                         htmlString += `<h4>${singleFriend.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friendshipId}">delete</button>`
-                                        // console.log(htmlString)
                                         document.querySelector("#frnds-list").innerHTML = htmlString;
                                     })
                             })
                         }
+                        //If the user has no friends, print the following
                         else {
                             htmlString = "add a friend to get started"
                             document.querySelector("#frnds-list").innerHTML = htmlString;
@@ -70,32 +70,6 @@ const buildFriends = (userId) => {
                     )
             }
         })
-    // .then(() => {
-    // APIManager.getAllFriendsByUser(userId)
-    //     .then(friends => {
-    //         console.log("friends inside of friend by user", friends)
-    //         if (friends.length > 0){
-    //         friends.forEach(friend => {
-    //             const otherFriendId = friend.otherFriendId
-    //             const friendshipId = friend.id
-
-    //             APIManager.getSingleFriendbyId(otherFriendId)
-    //                 .then((singleFriend) => {
-    //                     htmlString += `<h4>${singleFriend.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friendshipId}">delete</button>`
-    //                     // console.log(htmlString)
-    //                     document.querySelector("#frnds-list").innerHTML = htmlString;
-    //                 })
-
-    //         })
-    //     }
-    //     else {
-    //         htmlString = htmlString
-    //         document.querySelector("#frnds-list").innerHTML = htmlString;
-    //     }
-
-
-    //     })
-    // })
 
 }
 
