2b4472ed444500660c1e5777f2e6f5491d646201
sydneywait <sydneywait@gmail.com>
2019-03-01 10:06:04 -0500

    edited bug fixes in enter event listener in tasks

--- src/scripts/Tasks/tasksFormsBuilder.js
+++ src/scripts/Tasks/tasksFormsBuilder.js
@@ -10,8 +10,8 @@ const forms = {
     editTaskForm: (taskObject) => {
         console.log(taskObject)
         return `<h3>Edit Task</h3>
-<input type="textarea" name="task" id="task-name-input-${taskObject.id}" placeholder="Task" value="${taskObject.task}"></input>
-<input type="date" name="dueDate" id="task-date-input-${taskObject.id}" value="${taskObject.dueDate}"></input>`
+<input type="textarea" name="task" id="task-edit-name-input-${taskObject.id}" placeholder="Task" value="${taskObject.task}"></input>
+<input type="date" name="dueDate" id="task-edit-date-input-${taskObject.id}" value="${taskObject.dueDate}"></input>`
     },
     saveButton: (userId) => { //save button html
         return `<button type="button" class="save-new" id="save-tasks-btn-${userId}">Save</button>`
