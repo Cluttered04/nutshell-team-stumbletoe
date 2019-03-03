3c767ac2fa904b89e2cda690441f34c666a740bb
Michelle Tabor <mtabor66@outlook.com>
2019-03-03 18:31:07 -0500

    everything is awful

--- src/scripts/friends/friendBuilder.js
+++ src/scripts/friends/friendBuilder.js
@@ -1,76 +1,80 @@
 //This module goes and finds all the friend relationships of a single user and posts to dom
 // built by Sydney Wait
 
-import APIManager from "./friendAPIManager"
-
-
+let htmlString = "<h2>FRIENDS</h2>"
 const buildFriends = (userId) => {
-    let htmlString = "<h2>FRIENDS</h2>"
     // Get all the friends when the user is in the otherFriendId place
-    APIManager.getAllFriendsByFriend(userId)
+    fetch(`http://localhost:8088/friends?userId=${userId}`)
+        .then(r => r.json())
         .then((friends) => {
-            // If there are any friends assigned to the user, continue
+            // Check to see if there are any. If so, get their Ids
             if (friends.length > 0) {
-
                 friends.forEach(friend => {
-                    htmlString += `<h4>${friend.user.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friend.id}">delete</button>`
-                    document.querySelector("#frnds-list").innerHTML = htmlString;
+                    const otherFriendId = friend.otherFriendId
+                    const friendshipId = friend.id
+                    // use the ids to get them individually
+                    fetch(`http://localhost:8088/friends?userId=${otherFriendId}`)
+                        .then(r => r.json())
+                        .then((singleFriend) => {
+                            friendPartOne = `<button type="submit" class = "btn" id ="del-frnds-btn-${friendshipId}">delete</button><span><strong>${singleFriend.username}</strong></span>`
+                            // document.querySelector("#frnds-list").innerHTML = htmlString;
+                            fetch(`http://localhost:8088/events?userId=${otherFriendId}`)
+                                .then(r => r.json())
+                                .then((events) => {
+                                    if (events.length > 0) {
+                                        events.forEach((singleEvent) => {
+                                            let eventPartOne = `<li>${singleEvent.name}</li>`
+                                            console.log(eventPartOne)
+                                        })
+                                        fetch(`http://localhost:8088/news?userId=${otherFriendId}`)
+                                            .then(r => r.json())
+                                            .then((news) => {
+                                                if (news.length > 0) {
+                                                    news.forEach(singleArticle => {
+                                                        htmlString += `<li>${singleArticle.title}</li>`
+                                                        console.log(htmlString)
+                                                        document.querySelector("#frnds-list").innerHTML = htmlString;
+                                                    })
+                                                } else {
+                                                    htmlString += `<p>No News</p>`
+                                                    console.log(htmlString)
+                                                    document.querySelector("#frnds-list").innerHTML = htmlString;
+                                                }
+                                            })
+                                    } else {
+                                        htmlString += `<li>No Events.</li>`
+                                        console.log(htmlString)
+                                        fetch(`http://localhost:8088/news?userId=${otherFriendId}`)
+                                            .then(r => r.json())
+                                            .then((news) => {
+                                                if (news.length > 0) {
+                                                    news.forEach(singleArticle => {
+                                                        htmlString += `<li>${singleArticle.title}</li>`
+                                                        console.log(htmlString)
+                                                        document.querySelector("#frnds-list").innerHTML = htmlString;
+                                                    })
+                                                } else {
+                                                    htmlString += `<p>No News</p>`
+                                                    console.log(htmlString)
+                                                    document.querySelector("#frnds-list").innerHTML = htmlString;
+                                                }
+                                            })
+
+                                    }
+                                })
+
+                        })
                 })
-                // get all the friends when the user is in the userId place
-                APIManager.getAllFriendsByUser(userId)
-                    .then((friends) => {
-                        // Check to see if there are any. If so, get their Ids
-                        if (friends.length > 0) {
-                            friends.forEach(friend => {
-                                const otherFriendId = friend.otherFriendId
-                                const friendshipId = friend.id
-                                // use the ids to get them individually
-                                APIManager.getSingleFriendbyId(otherFriendId)
-                                    .then((singleFriend) => {
-                                        htmlString += `<h4>${singleFriend.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friendshipId}">delete</button>`
-                                        document.querySelector("#frnds-list").innerHTML = htmlString;
-                                    })
-                            })
-                        }
-                        else {
-                            // otherwise, leave the HTML string unchanged
 
-                            htmlString = htmlString
-                            document.querySelector("#frnds-list").innerHTML = htmlString;
-                        }
+            } else {
+                // otherwise, leave the HTML string unchanged
 
-                    })
-
+                htmlString += `<p>Add some friends to get started</p>`
+                document.querySelector("#frnds-list").innerHTML = htmlString;
             }
-            else {
-                // If the user is never in the otherFriendId place, check the userId place by itself
-                APIManager.getAllFriendsByUser(userId)
-                    .then(friends => {
-                        // if they have friends, get ids
-                        if (friends.length > 0) {
-                            friends.forEach(friend => {
-                                const otherFriendId = friend.otherFriendId
-                                const friendshipId = friend.id
-                                // use ids to look up friends and make a list
-                                APIManager.getSingleFriendbyId(otherFriendId)
-                                    .then((singleFriend) => {
-                                        htmlString += `<h4>${singleFriend.username}<h4><button type="submit" class = "btn" id ="del-frnds-btn-${friendshipId}">delete</button>`
-                                        document.querySelector("#frnds-list").innerHTML = htmlString;
-                                    })
-                            })
-                        }
-                        //If the user has no friends, print the following
-                        else {
-                            htmlString = "<h2>FRIENDS</h2>add a friend to get started"
-                            document.querySelector("#frnds-list").innerHTML = htmlString;
-                        }
-                    }
 
-
-                    )
-            }
         })
-
 }
+
 
 export default buildFriends
